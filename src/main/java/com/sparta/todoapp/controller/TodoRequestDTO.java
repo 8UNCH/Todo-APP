b5960ed4f07f4f89373ebe2e73d0db43a203438a
8UNCH <165640107+8UNCH@users.noreply.github.com>
2024-07-05 13:22:05 +0900

    레퍼지토리생성

--- src/main/java/com/sparta/todoapp/controller/TodoRequestDTO.java
+++ src/main/java/com/sparta/todoapp/controller/TodoRequestDTO.java
@@ -1,4 +1,25 @@
 package com.sparta.todoapp.controller;
 
-public class TodoRequestDTO {
+import com.sparta.todoapp.repository.Todo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+ public class TodoRequestDTO {
+    private String title;
+
+    private String content;
+
+    private String userName;
+
+    private String password;
+
+    public Todo toEntity() {
+        return Todo.builder()
+                .title(title)
+                .content(content)
+                .userName(userName)
+                .password(password)
+                .build();
 }
--- src/main/java/com/sparta/todoapp/repository/Todo.java
+++ src/main/java/com/sparta/todoapp/repository/Todo.java
@@ -1,4 +1,31 @@
 package com.sparta.todoapp.repository;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.ErrorResponse;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "todo_id", nullable = false)
+    private Long todoId;
+    private String content;
+
+    private String userName;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Todo(string)
+
 }
